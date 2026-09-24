b05b22112996dd0dd3ae204898e482790cb72a3c
galliganowen9-cmd <galliganowen9@gmail.com>
2026-09-24 16:39:06 -0400

    Update bridge

--- bridge.py
+++ bridge.py
@@ -7,13 +7,16 @@ from threading import Event
 
 ackinput = DigitalInputDevice(17, pull_up=False)
 ack2input = DigitalInputDevice(27, pull_up=False)
+ack3input = DigitalInputDevice(22, pull_up=False)
 HEADER_FILE = "autoimagedata.h" 
 PORT = "/dev/ttyUSB0"
 BAUD = 1000000
 ack1_seen = Event()
 ack2_seen = Event()
+ack3_seen = Event()
 ackinput.when_activated = lambda: ack1_seen.set()
 ack2input.when_activated = lambda: ack2_seen.set()
+ack3input.when_activated = lambda: ack3_seen.set()
 with open(HEADER_FILE, "r") as f:
 	text = f.read()
 
@@ -51,5 +54,8 @@ for i in range(0,len(data),4096):
 		break
 	if not ack2_seen.wait(timeout=5):
 		print("ACK 2 timeout")
+		break
+	if not ack3_seen.wait(timeout=5):
+		print("ACK 3 timeout")
 		break
 	print("Sending next")
